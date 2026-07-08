500e0e1d6810a5305ed869132d2b298cbc799d01
doddsi <ionadodds@gmail.com>
2026-07-08 22:50:52 +0100

    stops failing purely because of an unused import or variable sitting around

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 function App() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-[#d5573b]">
-      <h1 className="text-[18vw] font-white leading-none tracking-tighter text-white sm:text-[15vw]">
+      <h1 className="text-[18vw] font-black leading-none tracking-tighter text-white sm:text-[15vw]">
         CHOOSE
       </h1>
       <h1 className="text-[18vw] font-black leading-none tracking-tighter text-white sm:text-[15vw]">
         PERSEVERE
       </h1>
-      <h2 className="text-white ">Coming Soon.</h2>
+      <h2 className="text-white">Coming Soon.</h2>
     </div>
   );
 }
